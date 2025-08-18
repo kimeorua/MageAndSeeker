26628c84a09b164a6ec8bba84a6b95ab148bd4a9
kimeorua <kimeorua1222@naver.com>
2025-08-18 10:19:44 +0900

    Save & Load Subsystem Class 추가

---
+++ Source/MageAndSeeker/Public/Subsystem/SaveLoadSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SaveLoadSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGEANDSEEKER_API USaveLoadSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
